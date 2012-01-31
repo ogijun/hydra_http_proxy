c7aebfa0d15f10ccd1a44fc7b132e5897d009778
Junya Ogino <ogijun@gmail.com>
2012-01-31 18:56:18 +0900

    Project created.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GetMock::Application.config.secret_token = '7ca0bf6d7a94267f67841ff96b29c8cdf056d4a71cf8c5f9906a760aa79d640d7873cffe8b778884d84c10c6e98dc6eae2d0bf44a6c74a01772d6a0dfbdd3a8e'
