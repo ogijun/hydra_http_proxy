f4fd8a872239ec167b6e4d5d89db49c562f05bed
Junya Ogino <ogijun@gmail.com>
2012-03-09 18:40:16 +0900

    Console added.

--- app/controllers/console_controller.rb
+++ app/controllers/console_controller.rb
@@ -3,7 +3,18 @@ class ConsoleController < ApplicationController
   end
 
   def put_bundle
-    
+    @json = params[:json]
+    begin
+      @data = JSON.parse(@json)
+      if @data['bundle'].present?
+        job_bundle = JobBundle.new @data
+        morphed = job_bundle.morph
+        morphed.enqueue
+        @saved_key = morphed.save $redis
+      end
+    rescue JSON::ParserError => e
+      @data = 'Not JSON'
+    end
   end
 
   def get_result
