6f9d378cecd645827e3f875d1f3187d54da8d741
Junya Ogino <ogijun@gmail.com>
2012-06-07 17:49:25 +0900

    Rename app.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-GetMock::Application.config.session_store :cookie_store, key: '_get_mock_session'
+GetMock::Application.config.session_store :cookie_store, :key => '_hydra_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
