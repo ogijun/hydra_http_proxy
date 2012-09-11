6b80650d9c7956f01eaaa820aa6eda9e70125b8f
Junya Ogino <ogijun@gmail.com>
2012-09-11 17:38:42 +0900

    Changed xml definition.

--- app/filters/aucfan_item_job.rb
+++ app/filters/aucfan_item_job.rb
@@ -6,7 +6,7 @@ class AucfanItemJob < AbstractJob
   def aucfan_url
     auction_id = params[:auction_id]
     site = params[:site]
-    "http://aucfan.com/aucview/#{site}/#{auction_id}/?output=xml"
+    "http://60.36.190.45/aucview/#{site}/#{auction_id}/?output=xml"
   end
 
   require 'nokogiri'
@@ -17,17 +17,30 @@ class AucfanItemJob < AbstractJob
 
   def self.extract_row item
     {
-      :auction_id => item.at_css('auctionId').text,
-      :url => item.at_css('url').text,
+      :auctionId => item.at_css('auctionId').text,
       :title => item.at_css('title').text,
-      :text => item.at_css('caption').text,
-      :seller => item.at_css('seller id').text,
-      :start_price => item.at_css('beginningPrice').text.to_i,
-      :price => item.at_css('price').text.to_i,
-      :end_time => item.at_css('endDate').text,
+      :site => item.at_css('site').text,
+      :categoryId => item.at_css('categoryId').text,
+      :affiliateUrl => item.at_css('url').text,
+      :url => item.at_css('url').text,
+      :mobileUrl => item.at_css('url').text,
+      :limit => item.at_css('limit').text,
+      :beginningDate => item.at_css('beginningDate').text,
+      :endDate => item.at_css('endDate').text,
+      :beginningPrice => item.at_css('beginningPrice').text,
+      :price => item.at_css('price').text,
+      :amount => item.at_css('amount').text,
       :bid => item.at_css('bid').text.to_i,
-      :quantity => item.at_css('amount').text.to_i,
-      :pictures => item.at_css('images').children.map(&:text)
+      :bidUnit => item.at_css('bidUnit').text.to_i,
+      :seller => {
+        :id => item.at_css('seller id').text,
+        :evalUrl => item.at_css('seller evalUrl').text,
+        :mobileEvalUrl => item.at_css('seller mobileEvalUrl').text,
+        :mobileAffiliateEvalUrl => item.at_css('seller mobileAffiliateEvalUrl').text,
+        :mobileItemUrl => item.at_css('seller mobileItemUrl').text,
+        :mobileAffiliateItemUrl => item.at_css('seller mobileAffiliateItemUrl').text,
+      },
+      :images => item.at_css('images').children.map(&:text)
     }
   end
 end
